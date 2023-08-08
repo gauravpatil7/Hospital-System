649a91a22de137bb84b60cedc80461c6320686fe
Gaurav Patil <gauravpatil23602@gmail.com>
2023-08-09 00:53:45 +0530

    Scaffolding Done

---
+++ backend/HospitalWebApplication/Models/Appontment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalWebApplication.Models
+{
+    public partial class Appontment
+    {
+        public int Appontmentid { get; set; }
+        public int? Hospitalid { get; set; }
+        public string? Userid { get; set; }
+        public int? Doctorid { get; set; }
+        public DateTime? Appointmenttime { get; set; }
+
+        public virtual Doctor? Doctor { get; set; }
+        public virtual Hospital? Hospital { get; set; }
+        public virtual User? User { get; set; }
+    }
+}
---
+++ backend/HospitalWebApplication/Models/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HospitalWebApplication.Models
+{
+    public partial class Doctor
+    {
+        public Doctor()
+        {
+            Appontments = new HashSet<Appontment>();
+        }
+
+        public int Did { get; set; }
+        public string Dname { get; set; } = null!;
+        public string Degree { get; set; } = null!;
+        public byte? Experience { get; set; }
+        public int? Worksin { get; set; }
+        public string? Dimage { get; set; }
+
+        public virtual Hospital? WorksinNavigation { get; set; }
+        public virtual ICollection<Appontment> Appontments { get; set; }
+    }
+}
